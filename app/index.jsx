9fabe0b56a792eb71791485253309babe8e0da79
Metared04 <e.metared@gmail.com>
2025-03-20 10:02:22 +0100

    Premier commit

---
+++ app/index.jsx
@@ -0,0 +1,27 @@
+import { View, Text, Alert, Button, TextInput } from "react-native";
+import React from 'react';
+
+//------------------- data :
+
+class User
+{
+	constructor( userName = "Unknown", userEmail="Aucun")
+	{
+		this.userName = userName;
+		this.userEmail = userEmail;
+	}
+}
+
+export default function HomeScreen() {
+  return (
+    <View style={{ 
+      flex: 1, 
+      justifyContent: 'center', 
+      alignItems: 'center' }}
+      >
+        <Text>Bienvenue dans mon app React Native !</Text>
+        <Button onPress={() => console.log("Oui.")} title="lets go !"/>
+        <TextInput placeholder="Oui" editable textAlign="center"/>
+    </View>
+  );
+}
